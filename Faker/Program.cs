845508449f5adecd6a7bf7690392b782340e3aff
odlmon <dimaosipov.2000@mail.ru>
2020-09-27 13:18:15 +0300

    Initial commit

---
+++ Faker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Faker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
