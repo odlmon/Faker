03277cdccbf98a6a6f25ea0ebcc517a05cbf8e6c
odlmon <dimaosipov.2000@mail.ru>
2020-09-28 19:58:26 +0300

    Add DTO class

---
+++ Faker/Foo.cs
@@ -0,0 +1,19 @@
+﻿namespace Faker
+{
+    public class Foo
+    {
+        public int x;
+        public string y;
+
+        public Foo(int x)
+        {
+            this.x = x;
+        }
+
+        private Foo()
+        {
+            x = 1;
+            y = "1";
+        }
+    }
+}
